60e9040597234b2c365f93f6d0b8dff05cbf737a
pingchoy <pingchoy.1@gmail.com>
2024-11-07 16:49:04 +1100

    redirect to /dashboard by default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    // Redirect to /dashboard
+    if (window) {
+      window.location.href = "/dashboard";
+    }
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
