eca4e371488f9118b69281aea65a6d9558f7f1d4
Ping <pingchoy.1@gmail.com>
2025-02-04 00:01:14 +1100

    Update firebaseConfig.js

--- src/app/firebase/firebaseConfig.js
+++ src/app/firebase/firebaseConfig.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBvQkvm37cXoipHmcYgIRGGQXmpt6kEZTM",
+    apiKey: "xxx",
     authDomain: "dnd-app-9609f.firebaseapp.com",
     projectId: "dnd-app-9609f",
     storageBucket: "dnd-app-9609f.appspot.com",
